7d8321229ee206fa84f465a97e8880914e43049e
Thomas Stuttard <thomas.stuttard@softlabs.co.uk>
2012-02-02 15:01:55 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 SampleApp::Application.routes.draw do
 
 
+  get "users/new"
+
+  match '/signup',  to: 'users#new'
+
+
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
