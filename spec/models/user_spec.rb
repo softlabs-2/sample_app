36c9e58cbefd73fa629c355670633346bb301288
Thomas Stuttard <thomas.stuttard@softlabs.co.uk>
2012-02-06 10:05:09 +0000

    Commiting Test for user validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,12 +13,15 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do
+	   @user = User.new(name: "Example User", email: "user@example.com")
+	end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to (:password_digest) }
 
   it { should be_valid }
 
@@ -26,5 +29,41 @@ describe User do
   	before { @user.name = ""}
   	it { should_not be_valid}
   end
+
+  describe "when email is not present" do
+  	before {@user.email = " "}
+  	it { should_not be_valid}
+  end
+
+  describe "when email is too long" do
+  	before { @user.name = "a" * 51}
+  	it { should_not be_valid}
+  end
+
+  describe "when email format is invalid" do
+  	invalid_addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+  	invalid_addresses.each do |invalid_address|
+  		before {@user.email=invalid_address}
+  		it {should_not be_valid}
+  	end
+  end
+
+
+  	describe "when email format is valid" do
+  		valid_addresses = %w[user@foo.com A_USER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+  		valid_addresses.each do |valid_address|
+  			before { @user.email = valid_address}
+  			it { should be_valid}
+  		end
+  	end
+
+  	describe "when email address is already taken" do
+  		before do
+  			user_with_same_email = @user.dup
+  			user_with_same_email.email = @user.email.upcase
+  			user_with_same_email.save
+  		end
+  		it { should_not be_valid}
+  	end
 end
 
